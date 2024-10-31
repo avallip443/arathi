bd3135efad91df42b1ecc69d45a7244f14fc2e36
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-10-31 17:32:25 -0400

    create boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import "./App.css";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/nav/NavBar";
+import Projects from "./components/projects/Projects";
 
 function App() {
   return (
     <>
+      <NavBar />
+      <About />
+      <Experience />
+      <Projects />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
