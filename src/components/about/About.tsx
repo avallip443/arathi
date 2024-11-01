299fa4414ac6a244125e94aeca7ba4b5da35c0ca
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-10-31 20:28:32 -0400

    create about section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { Github, Linkedin } from "lucide-react";
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="py-6">
+      <div className="flex gap-8">
+        <div className="w-4/5">
+          <h1 className="text-5xl font-bold pb-5">hi, i'm arathi :)</h1>
+          <h4 className="text-lg pb-5">
+            computer science student @ toronto metropolitan university
+          </h4>
+          <p className="pb-8">
+            I like building full-stack applications, exploring new technologies,
+            and making tea.
+          </p>
+
+          <div className="flex gap-8 items-center">
+            <a href="mailto:avallpipuranathan@gmail.com">
+              <button className="px-6 py-2 border border-gray-500 rounded-lg hover:bg-gray-800">
+                Say hi
+              </button>
+            </a>
+            <a
+              href="https://github.com/avallip443"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Github className="text-gray-300 hover:text-white" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/avallip/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Linkedin className="text-gray-300 hover:text-white" />
+            </a>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <img src="images/me.jpeg" alt="me!" className="rounded-lg w-56" />
+        </div>
+      </div>
     </div>
   );
 };
