b2a348f50bf26b2968159d42b06837cea483e7bd
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-11-02 00:30:45 -0400

    add responsiveness

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,20 +4,22 @@ import { Github, Linkedin } from "lucide-react";
 const About: React.FC = () => {
   return (
     <div className="py-6">
-      <div className="flex gap-8">
-        <div className="w-4/5">
+      <div className="md:flex gap-8">
+        <div className="md:w-4/5">
           <h1 className="text-5xl pb-5">
             hi, i'm arathi :)
           </h1>
+
           <h4 className="text-lg pb-5">
             computer science student @ toronto metropolitan university
           </h4>
+
           <p className="text-gray-300 pb-8">
             I like building full-stack applications, exploring new technologies,
             and making tea.
           </p>
 
-          <div className="flex gap-8 items-center">
+          <div className="flex gap-6 md:gap-8 items-center">
             <a href="mailto:avallpipuranathan@gmail.com">
               <button className="px-6 py-2 border-2 border-gray-800 rounded-lg hover:bg-gray-800 hover:border-[#d2b7e5] hover:text-[#d2b7e5]">
                 Say hi
@@ -40,7 +42,7 @@ const About: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex items-center">
+        <div className="flex items-center justify-center mt-6 md:mt-0">
           <img src="images/me.jpeg" alt="me!" className="rounded-lg w-56" />
         </div>
       </div>
