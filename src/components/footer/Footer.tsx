2e892bb8e27ec6dbeb4bc21664f9e8a45e057b00
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-11-02 18:21:02 -0400

    add responsiveness

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center py-4 gap-y-4">
       <div className="flex items-center gap-x-6">
-        <hr className="w-56 h-0.5 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
+        <hr className="w-12 md:w-56 h-0.5 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
 
         <a href="mailto:avallpipuranathan@gmail.com">
           <Mail className="text-gray-300 hover:text-white hover:text-[#d2b7e5]" />
@@ -25,7 +25,7 @@ const Footer: React.FC = () => {
           <Linkedin className="text-gray-300 hover:text-[#d2b7e5]" />
         </a>
 
-        <hr className="w-56 h-0.5 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
+        <hr className="w-12 md:w-56 h-0.5 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
       </div>
       <p className="text-sm">Built by Arathi Vallipuranathan </p>
     </div>
