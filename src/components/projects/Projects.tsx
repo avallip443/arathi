3a820926b926e161db60c7b08fc3b1c38bdd81f1
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-12-19 22:08:07 -0500

    update project layout

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,7 +5,10 @@ const Projects: React.FC = () => {
   return (
     <div className="py-12">
       <h1 className="text-4xl">projects</h1>
-      <ProjectsList />
+      <h2 className="text-2xl bg-gray-700 inline-block px-3 py-1 rounded-lg mt-4">personal</h2>
+      <ProjectsList type={"personal"} />
+      <h2 className="text-2xl bg-gray-700 inline-block px-3 py-1 rounded-lg mt-8">academic</h2>
+      <ProjectsList type={"academic"} />
     </div>
   );
 };
